a3246a60ce0554a98b4dc70b04c5e8ac761fa2ef
Gabriel Coradini <gabrielcoradini3@gmail.com>
2026-04-18 15:54:58 -0300

    feat(api): implement CreateSale endpoint with request/response mapping
    - Added Sales feature following project pattern
    - Created CreateSale request, response, validator and profile
    - Implemented SalesController using MediatR

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleProfile.cs
@@ -0,0 +1,12 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale;
+using AutoMapper;
+
+public class CreateSaleProfile : Profile
+{
+    public CreateSaleProfile()
+    {
+        CreateMap<CreateSaleRequest, CreateSaleCommand>();
+        CreateMap<CreateSaleItemRequest, CreateSaleItemCommand>();
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/SaleController.cs
@@ -0,0 +1,29 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SalesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public SalesController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateSaleRequest request)
+    {
+        var command = _mapper.Map<CreateSaleCommand>(request);
+
+        var result = await _mediator.Send(command);
+
+        return Ok(new CreateSaleResponse { Id = result });
+    }
+}
